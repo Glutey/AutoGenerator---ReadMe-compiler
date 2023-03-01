84a1d1b30c703d2789b6b978a234555e2728e9e2
Tom-Stranger <115110026+Glutey@users.noreply.github.com>
2023-03-01 20:32:52 +0000

    Update generateMarkdown.js
    removed all of the html elements and after looking at a markdwn language sheet i have ensured that the generated markdown will be as required.. and not a strange html hybrid.

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,52 +1,35 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-//   return `<!DOCTYPE html>
-//   <html lang="en">
-//   <head>
-//       <meta charset="UTF-8">
-//       <meta http-equiv="X-UA-Compatible" content="IE=edge">
-//       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>${data.title}</title>
-//   </head>
-//   <body>
+   return `${data.title}
       **Table of Contents**
-      <ol>
-        <li href="#description">Description</li>
-        <li href="#install">Installation</li>
-        <li href="#usage">Usage Info</li>
-        <li href="#guide">Contribution Guidelines</li>
-        <li href="#test">Testing</li>
-        <li href="#quest">Questions</li>
-        <br>
+        1.Description
+        2.Installation
+        3.Usage Info
+        4.Contribution Guidelines
+        5.Testing
+        6.Questions
+        
       
       #Description
-      ~~${data.description}~~
-      <br>
+      ${data.description}
       
-      ##Installation
-      ~~This program works by ${data.install}~~
-      <br>
+      #Installation
+      This program works by ${data.install}
       
       ##Usage Information
-      ~~${data.usage}~~
-      <br>
+      ${data.usage}
       
-      ###Contribution Guidelines
-      ~~${data.guide}~~
-      <br>
+      ##Contribution Guidelines
+      ${data.guide}
       
       ###Testing
-      ~~${data.test}~~
-      <br>
+      ${data.test}
      
       ###Questions
-      <br>
-      ~~For further information, help or advice, please use one of the following contact methods~~
-      ~~Email: ${data.email}~~
-      ~~GitHub: ${data.github}~~
-      ~~LinkedIn: https://www.linkedin.com/in/${data.linkedin}~~
-//   </body>
-//   </html>`
+      For further information, help or advice, please use one of the following contact methods
+      Email: ${data.email}
+      GitHub: ${data.github}
+      LinkedIn: https://www.linkedin.com/in/${data.linkedin}
 ; 
 }
 module.exports = generateMarkdown;
