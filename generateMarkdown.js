be71c65638f6ddfe38fef6db12aad3d371d8ec3c
Tom-Stranger <115110026+Glutey@users.noreply.github.com>
2023-03-01 10:46:30 +0000

    Updated the Markdown
    After running the program, the resulting README file was generated in HTML.. the amendments i introduced here should clear the HTML and instead return a more markdown style document.

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,14 +1,14 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `<!DOCTYPE html>
-  <html lang="en">
-  <head>
-      <meta charset="UTF-8">
-      <meta http-equiv="X-UA-Compatible" content="IE=edge">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//   return `<!DOCTYPE html>
+//   <html lang="en">
+//   <head>
+//       <meta charset="UTF-8">
+//       <meta http-equiv="X-UA-Compatible" content="IE=edge">
+//       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <title>${data.title}</title>
-  </head>
-  <body>
+//   </head>
+//   <body>
       **Table of Contents**
       <ol>
         <li href="#description">Description</li>
@@ -19,34 +19,34 @@ function generateMarkdown(data) {
         <li href="#quest">Questions</li>
         <br>
       
-      <h1 id="description>#Description</h1>
-      <p>${data.description}</p>
+      #Description
+      ~~${data.description}~~
       <br>
       
-      <h2 id="install">#Installation</h2>
-      <p>This program works by ${data.install}</p>
+      ##Installation
+      ~~This program works by ${data.install}~~
       <br>
       
-      <h2 id="usage">#Usage Information</h2>
-      <p>${data.usage}</p>
+      ##Usage Information
+      ~~${data.usage}~~
       <br>
       
-      <h3 id="guide">#Contribution Guidelines</h2>
-      <p>${data.guide}</p>
+      ###Contribution Guidelines
+      ~~${data.guide}~~
       <br>
       
-      <h3 id="test">#Testing</h3>
-      <p>${data.test}</p>
+      ###Testing
+      ~~${data.test}~~
       <br>
      
-      <h3 id="quest"> #Questions</h3>
+      ###Questions
       <br>
-      <p>For further information, help or advice, please use one of the following contact methods<p>
-      <p>Email: ${data.email}</p>
-      <p>GitHub: ${data.github}</p>
-      <p>LinkedIn: https://www.linkedin.com/in/${data.linkedin}</p>
-  </body>
-  </html>`
+      ~~For further information, help or advice, please use one of the following contact methods~~
+      ~~Email: ${data.email}~~
+      ~~GitHub: ${data.github}~~
+      ~~LinkedIn: https://www.linkedin.com/in/${data.linkedin}~~
+//   </body>
+//   </html>`
 ; 
 }
 module.exports = generateMarkdown;
